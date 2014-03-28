06e6bc0ad4e5387c0c557964ec791f0a4afd7973
Fernando Aramendi <fernando@devartis.com>
2014-03-28 16:35:57 -0300

    first commit

---
+++ devartis-admin/templatetags/devartis_admin.py
@@ -0,0 +1,11 @@
+from django.template import Library
+from django.contrib.sites.models import Site
+ 
+register = Library()
+
+
+@register.simple_tag(takes_context = True)
+def get_current_site_name(context):
+
+    return Site.objects.get_current().name
+    
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from admin_tools import VERSION
+
+github_url = 'https://github.com/devartis/django-devartis-admin'
+long_desc = '''
+%s
+
+%s
+''' % (open('README').read(), open('CHANGELOG').read())
+
+setup(
+    name='django-devartis-admin',
+    version=VERSION.replace(' ', '-'),
+    description='A devartis theme for the django admin site',
+    long_description=long_desc,
+    author='Fernando Aramendi',
+    author_email='fernando@devartis.com',
+    url=github_url,
+    download_url='%/archive/django-devartis-admin-%s.tar.gz' % (github_url, VERSION),
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    zip_safe=False,
+)
