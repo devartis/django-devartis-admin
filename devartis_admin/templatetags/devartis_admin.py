047924f04c1a0623d77c81a9e34892ef8cb6769c
Poli García <poli@devartis.com>
2014-10-16 08:18:13 -0300

    Add template tag get_current_site_domain

--- devartis_admin/templatetags/devartis_admin.py
+++ devartis_admin/templatetags/devartis_admin.py
@@ -8,4 +8,10 @@ register = Library()
 def get_current_site_name(context):
 
     return Site.objects.get_current().name
+
+
+@register.simple_tag(takes_context = True)
+def get_current_site_domain(context):
+
+    return Site.objects.get_current().domain
     
